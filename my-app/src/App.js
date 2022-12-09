172c90e3d552d782485d0008e2b5ebcd7fb5e01b
davidbriseo <david.briseo@gmail.com>
2022-12-09 00:51:05 -0600

    Initial file upload

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SearchBar />
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/IpInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function IpInfo(){
+    return(
+        <div className="ipInfo">
+            IP Info
+        </div>
+    )
+}
+
+export default IpInfo
